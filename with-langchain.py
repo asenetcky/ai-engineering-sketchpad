e02a68040a5eab4184a5fcc045bb5cfd20960444
asenetcky <me@asenetcky.dev>
2026-02-24 04:45:04 -0500

    feat: add langchain document loading

--- with-langchain.py
+++ with-langchain.py
@@ -1,12 +1,16 @@
 # /// script
 # dependencies = [
+#     "langchain==1.2.10",
+#     "langchain-community==0.4.1",
 #     "langchain-core==1.2.15",
 #     "langchain-openai==1.1.10",
+#     "langgraph==1.0.9",
 #     "marimo",
 #     "openai==2.23.0",
 #     "polars==1.38.1",
 #     "pydantic-ai==1.63.0",
 #     "python-dotenv==1.2.1",
+#     "wikipedia==1.4.0",
 # ]
 # requires-python = ">=3.13"
 # ///
@@ -36,6 +40,7 @@ def _():
         StrOutputParser,
         load_dotenv,
         os,
+        pl,
     )
 
 
@@ -157,6 +162,134 @@ def _(StrOutputParser, learning_prompt, llm, time_prompt):
     )
 
     print(_seq_chain.invoke({"activity": "program in Rust"}))
+    return
+
+
+@app.cell
+def _(llm):
+    # agents
+    from langchain.agents import create_agent
+    from langchain_community.agent_toolkits.load_tools import load_tools
+
+    tools = load_tools(["wikipedia"])
+
+    _agent = create_agent(llm, tools)
+
+    _response = _agent.invoke(
+        {"messages": [("human", "How many people live in New York City")]}
+    )
+
+    print(_response["messages"][-1].content)
+    return (create_agent,)
+
+
+@app.cell
+def _(pl):
+    customers_dict = {
+        "id": [101, 102, 103, 104, 105, 106, 107, 108, 109, 110],
+        "name": [
+            "Tech Innovators Inc.",
+            "Green Solutions Ltd.",
+            "Global Enterprises",
+            "Peak Performance Co.",
+            "Visionary Ventures",
+            "NextGen Technologies",
+            "Dynamic Dynamics LLC",
+            "Infinity Services",
+            "Eco-Friendly Products",
+            "Future Insights",
+        ],
+        "subscription_type": [
+            "Premium",
+            "Standard",
+            "Basic",
+            "Premium",
+            "Standard",
+            "Basic",
+            "Premium",
+            "Standard",
+            "Basic",
+            "Premium",
+        ],
+        "active_users": [450, 300, 150, 800, 600, 200, 700, 500, 100, 900],
+        "auto_renewal": [
+            True,
+            False,
+            True,
+            True,
+            False,
+            True,
+            True,
+            False,
+            True,
+            True,
+        ],
+    }
+
+    customers = pl.DataFrame(customers_dict)
+    customers.head()
+    return (customers,)
+
+
+@app.cell
+def _(customers, pl):
+    customers.filter(pl.col("name").eq("Peak Performance Co.")).to_dicts()
+    return
+
+
+@app.cell
+def _(customers, pl):
+    from langchain_core.tools import tool
+
+
+    @tool
+    def retrieve_customer_info(name: str) -> str:
+        """Retrieve customer information based on their name."""
+
+        return customers.filter(pl.col("name").eq(name)).to_dicts()
+
+    return (retrieve_customer_info,)
+
+
+@app.cell
+def _(retrieve_customer_info):
+    print(retrieve_customer_info.args)
+    return
+
+
+@app.cell
+def _(create_agent, llm, retrieve_customer_info):
+    _agent = create_agent(llm, [retrieve_customer_info])
+
+    _messages = _agent.invoke(
+        {
+            "messages": [
+                ("human", "Create a summary of our customer: Peak Performance Co.")
+            ]
+        }
+    )
+
+    print(_messages["messages"][-1].content)
+    return
+
+
+@app.cell
+def _(customers):
+    customers.write_csv("customers.csv")
+    return
+
+
+@app.cell
+def _():
+    # document loaders for rag
+    # from langchain_community.document_loaders import PyPDFLoader
+    from langchain_community.document_loaders.csv_loader import CSVLoader
+
+    _loader = CSVLoader("customers.csv")
+    _data = _loader.load()
+    print(_data)
+
+    print(_data[0].metadata)
     return
 
 
